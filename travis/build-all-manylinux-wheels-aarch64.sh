2be33d1664117d5c52eb9e8648b3dee655ef3f8f
sridish123 <82566673+sridish123@users.noreply.github.com>
2021-05-17 18:35:07 +0530

    Create build-all-manylinux-wheels-aarch64.sh

---
+++ travis/build-all-manylinux-wheels-aarch64.sh
@@ -0,0 +1,11 @@
+set -x
+set -e
+
+# Wait for docker pull to complete downloading container
+manylinux_image="ghcr.io/pyca/cryptography-manylinux2014_aarch64"
+docker pull "${manylinux_image}" &
+wait
+
+# Build wheels
+docker run --rm -v `pwd`:/io/src "${manylinux_image}" /io/src/travis/build-manylinux-wheels.sh &
+wait

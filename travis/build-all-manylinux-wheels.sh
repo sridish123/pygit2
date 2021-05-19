625e84009d617b9569099d7786dd6104caa8318e
sridish123 <82566673+sridish123@users.noreply.github.com>
2021-05-19 11:36:21 +0530

    Update build-all-manylinux-wheels.sh

--- travis/build-all-manylinux-wheels.sh
+++ travis/build-all-manylinux-wheels.sh
@@ -8,8 +8,12 @@ ARCH=$1
 # Wait for docker pull to complete downloading container
 if [ $ARCH == 'aarch64' ]; then
   manylinux_image="ghcr.io/pyca/cryptography-manylinux2014_${ARCH}"
+  echo "inside aarch64"
+  echo $manylinux_image
 else
   manylinux_image="ghcr.io/pyca/cryptography-manylinux2014:${ARCH}"
+  echo "inside x86"
+  echo $manylinux_image
 fi
 docker pull "${manylinux_image}" &
 wait

b7bbe4f22ec9046a302f74474dd997c7d6faf739
sridish123 <82566673+sridish123@users.noreply.github.com>
2021-05-19 10:23:54 +0530

    Update build-all-manylinux-wheels.sh

--- travis/build-all-manylinux-wheels.sh
+++ travis/build-all-manylinux-wheels.sh
@@ -3,7 +3,11 @@
 set -x
 set -e
 
+echo $1
+
 ARCH="${1:-x86_64}"
+
+echo $ARCH
 
 # Wait for docker pull to complete downloading container
 manylinux_image="ghcr.io/pyca/cryptography-manylinux2014${ARCH}"
